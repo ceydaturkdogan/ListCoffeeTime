5c90d347e35e333317841176c4d8aacdd09edc50
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-20 12:12:35 +0300

    Add project files.

---
+++ ListCoffeeTime/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> coffee=new List<string>(5);
+
+Console.WriteLine("Lütfen 5 adet kahve ismi giriniz");
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Kahve {i+1}:");
+    string coffeeItem = Console.ReadLine();
+    coffee.Add(coffeeItem);
+}
+
+Console.WriteLine("Girilen Kahve İsimleri");
+
+foreach (string item in coffee)
+{
+    Console.WriteLine(item); 
+}
